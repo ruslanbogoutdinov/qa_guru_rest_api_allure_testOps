861335e2e10c163f3a2cd2c9a1f7d7c9cfd6e1ac
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-04-03 22:38:59 +0500

    added Auth

--- src/test/java/tests/manual/Auth.java
+++ src/test/java/tests/manual/Auth.java
@@ -1,6 +1,13 @@
 package tests.manual;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+
+@Feature("Manual login tests")
 public class Auth {
+    @Test
+    @Story("Authorization")
     public void authTest(){
 
     }
