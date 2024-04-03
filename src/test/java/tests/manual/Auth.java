5be9b05081e4caf0594ef4714faf40701bcbdcb0
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-04-03 22:43:54 +0500

    automated Auth test

--- src/test/java/tests/manual/Auth.java
+++ src/test/java/tests/manual/Auth.java
@@ -1,14 +1,31 @@
 package tests.manual;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@Feature("Manual login tests")
+import static io.qameta.allure.Allure.step;
+
 public class Auth {
     @Test
+    @AllureId("31437")
+    @DisplayName("Correct auth")
     @Story("Authorization")
-    public void authTest(){
-
+    @Owner("allure8")
+    @Feature("Manual login tests")
+    public void authTest() {
+        step("Open app", () ->{
+            Assertions.assertTrue(true);
+        });
+        step("Enter correct pin-code", () -> {
+            Assertions.assertTrue(true);
+        });
+        step("Check that main page is opened", () -> {
+            Assertions.assertTrue(true);
+        });
     }
 }
